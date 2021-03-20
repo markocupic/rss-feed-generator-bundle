457ba5daa6b9a3f5c227a163c72640613f91a47f
Marko Cupic <m.cupic@gmx.ch>
2021-03-20 22:55:01 +0100

    dev

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of RSS Feed GeneratorBundle.
+ * 
+ * (c) Marko Cupic 2021 <m.cupic@gmx.ch>
+ * @license MIT
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/markocupic/rss-feed-generator-bundle
+ */
+
+namespace Markocupic\RssFeedGeneratorBundle\ContaoManager;
+
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+/**
+ * Class Plugin
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * @return array
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create('Markocupic\RssFeedGeneratorBundle\MarkocupicRssFeedGeneratorBundle')
+                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle']),
+        ];
+    }
+}
