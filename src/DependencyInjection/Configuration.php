338f38ddbaea054c749aa2a01127199fe16af0cd
Marko Cupic <m.cupic@gmx.ch>
2022-04-27 11:39:15 +0200

    Fix https://github.com/markocupic/rss-feed-generator-bundle/issues/1

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 /*
- * This file is part of RSS Feed GeneratorBundle.
+ * This file is part of RSS Feed Generator Bundle.
  *
- * (c) Marko Cupic 2021 <m.cupic@gmx.ch>
- * @license MIT
+ * (c) Marko Cupic 2022 <m.cupic@gmx.ch>
+ * @license GPL-3.0-or-later
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
  * @link https://github.com/markocupic/rss-feed-generator-bundle
@@ -19,7 +19,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    const ROOT_KEY = 'markocupic_rss_feed_generator';
+    public const ROOT_KEY = 'markocupic_rss_feed_generator';
 
     public function getConfigTreeBuilder()
     {
